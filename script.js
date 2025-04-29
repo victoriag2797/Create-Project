f9a4bce964718ea7e2a7ee364b75780206d3fcb8
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-04-29 12:31:55 +0000

    init commit

--- script.js
+++ script.js
@@ -1,111 +1,108 @@
-let questionList = ["Pick a NHL jersey:", "Pick a movie:", "Pick a song:", "Pick a person:", "Pick a school subject:", "Pick a quote:", "Pick a long running TV show from the 2010s:", "Pick a cult-classic fandom:", "Pick a fear:", "Pick a season:", "How many siblings do you have?:", "Introvert or Extrovert?:", "Pick an outfit:", "Pick an element:", "Pick a classic NY food:", "Pick one of my brother's favorite movie:", "Pick a random word:"]
-let optaList = ["Minnestota Wild", "Dead Poets Society", "Phoebe Bridgers", "Nico Hishcier", "English", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"]
-let optbList = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"]
-let optcList = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"]
-let optdList = ["a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a"]
-
-const question = document.querySelector('.question-container');
-const opta = document.getElementById('opta');
-const optb = document.getElementById('optb');
-const optc = document.getElementById('optc');
-const optd = document.getElementById('optd');
-
-let scoreContainer = document.querySelector('.score-container')
-let score = 0;
-let number = 1
-let questionIndex = 0;
-let optaIndex = 0;
-let optbIndex = 0;
-let optcIndex = 0;
-let optdIndex = 0;
-
-
-```
-button.addEventListener("click", optFunct); 
-
-function optFunct () {
-    questionIndex += 1;
-    question.textContent = questionList[questionIndex];
-    score += 1;
-    optaIndex += 1;
-    optbIndex += 1;
-    optcIndex += 1;
-    optdIndex += 1;
-    opta.textContent = optaList[optaIndex];
-    optb.textContent = optbList[optbIndex];
-    optc.textContent = optcList[optcIndex];
-    optd.textContent = optdList[optdIndex];
-    number +=1;
-    scoreContainer.textContent = "Score: " + score;
-}
-
-```
-
-opta.addEventListener("click", optaFunct); 
-
-function optaFunct () {
-    questionIndex += 1;
-    question.textContent = questionList[questionIndex];
-    score += 1;
-    optaIndex += 1;
-    opta.textContent = optaList[optaIndex];
-    optb.textContent = optbList[optbIndex];
-    optc.textContent = optcList[optcIndex];
-    optd.textContent = optdList[optdIndex];
-    number +=1;
-    scoreContainer.textContent = "Score: " + score;
-}
+//List of questions
+let questionList = [
+    "Pick a NHL jersey:", 
+    "Pick a movie:", 
+    "Pick an artist:", 
+    "Pick a person:", 
+    "Pick a school subject:",  
+    "Pick a long running TV show from the 2010s:", 
+    "Pick a cult-classic fandom:", 
+    "Pick a fear:", 
+    "Pick a season:", 
+    "Pick an element:",
+    "How many siblings do you have?:", 
+    "Introvert or Extrovert?:",  
+    "Pick a classic NY food:", 
+    "Pick one of my brother's favorite movie:", 
+    "Pick a random word:"
+];
+//List of answers for each question
+let optAnswers = [
+    ["Seattle Kraken", "St. Louis Blues", "Washington Capitals", "Carolina Hurricanes"], 
+    ["Dead Poets Society", "Aftersun", "The Notebook", "Uncut Gems"],
+    ["Phoebe Bridgers", "Sufjan Stevens", "Olivia Rodrigo", "Drake"],
+    ["Nico Hischier", "Alex Vlasic", "Eric Staal", "Mark Stone"],
+    ["English", "History", "Science", "Math"],
+    ["House MD", "Supernatural", "Once Upon a Time", "Criminal Minds"], 
+    ["Star Wars/Star Trek", "Marvel/DC", "Lord of the Rings", "Harry Potter"],
+    ["Death", "Heights", "The Dark", "Spiders"],
+    ["Winter", "Autumn", "Spring", "Summer"],
+    ["Air", "Water", "Earth", "Fire"],
+    ["6+", "4-5", "1-3", "Only Child"],
+    ["Ambivert", "Extrovert", "Introvert", "No clue"], 
+    ["Pasta", "Pizza", "Water", "Bagel"],
+    ["Whiplash", "Inglorious Basterds", "Green Mile", "No Country for Old Men"],
+    ["Cadaver", "Epiphany", "Rambunctious", "Wall"],
     
-optb.addEventListener("click", optbFunct); 
+];
 
-function optbFunct () {
-    questionIndex += 1;
-    question.textContent = questionList[questionIndex];
-    score += 2;
-    optbIndex += 1;
-    opta.textContent = optaList[optaIndex];
-    optb.textContent = optbList[optbIndex];
-    optc.textContent = optcList[optcIndex];
-    optd.textContent = optdList[optdIndex];
-    number +=1;
-    scoreContainer.textContent = "Score: " + score;
+//Gets the IDs from the HTML and assigns them values that will not change
+const questionContainer = document.getElementById('question-container');
+const questionSection = document.getElementById('question-section');
+const answerContent = [
+    document.getElementById('opta-btn'),
+    document.getElementById('optb-btn'),
+    document.getElementById('optc-btn'),
+    document.getElementById('optd-btn'),
+];
+
+//Gets the IDs from the HTML and assigns them values that could change
+let scoreContainer = document.getElementById('score-container');
+let questionNumberContainer = document.getElementById('question-number');
+let score = 0;
+let questionIndex = 0;
+
+//
+function gradeAnswer(answerIndex) {
+    for (let i = 0; i < 4; i++){
+        if (i === answerIndex) {
+            score += i + 1;
+        }
+    }
+    optFunct();
 }
 
-optc.addEventListener("click", optcFunct); 
 
-function optcFunct () {
-    questionIndex += 1;
-    question.textContent = questionList[questionIndex];
-    score += 3;
-    optcIndex += 1;
-    opta.textContent = optaList[optaIndex];
-    optb.textContent = optbList[optbIndex];
-    optc.textContent = optcList[optcIndex];
-    optd.textContent = optdList[optdIndex];
-    number +=1;
-    scoreContainer.textContent = "Score: " + score;
-}
+function optFunct() {
+    if (questionIndex < questionList.length) {
+        questionNumberContainer.textContent = `Question Number: ${
+            questionIndex + 1
+        }`;
+        questionContainer.textContent = questionList[questionIndex];
+        answerContent[0].textContent = optAnswers[questionIndex][0];
+        answerContent[1].textContent = optAnswers[questionIndex][1];
+        answerContent[2].textContent = optAnswers[questionIndex][2];
+        answerContent[3].textContent = optAnswers[questionIndex][3];
+        questionIndex += 1;
+    } else {
+        questionSection.textContent = "";
+        scoreContainer.innerText = "Your score is ${score}/60";
+        if (score == 15) {
+            scoreContainer.innerText = "I fear you are literally me, exactly. Like we picked the exact same answers. You're perfect!";
+            } else if (score > 15 && score < 25) {
+                scoreContainer.innerText = "We have really similar opinions and we would probably get along really well!";
+            } else if (score > 24 && score < 34) {
+                scoreContainer.innerText = "We are pretty similar. You have good taste (mostly).";
+            } else if (score > 34 && score < 44) {
+                scoreContainer.innerText = "Down the middle. Your opinions are half good, half bad.";
+            } else if (score > 43 && score < 53) {
+                scoreContainer.innerText = "We are pretty different. You have some good opinions though (Not a lot).";
+            } else if (score > 52 && score < 59) {
+                scoreContainer.innerText = "We're very different. You need to get some better opinions.";
+            } else (score == 60) {
+                scoreContainer.innerText = "We're exact opposites. We picked the opposite answer every time. You're heavily flawed (joking (mostly)).";
+            }
+        }
+};
 
-optd.addEventListener("click", optdFunct); 
+optFunct();
 
-function optdFunct () {
-    questionIndex += 1;
-    question.textContent = questionList[questionIndex];
-    score += 4;
-    optdIndex += 1;
-    opta.textContent = optaList[optaIndex];
-    optb.textContent = optbList[optbIndex];
-    optc.textContent = optcList[optcIndex];
-    optd.textContent = optdList[optdIndex];
-    number +=1;
-    scoreContainer.textContent = "Score: " + score;
-}
-
-if (score = 20) {
-    console.log ("I fear you are literally me, exactly. Like we picked the exact same answers.");
-  } if (score > 20) {
-    greeting = "Good evening";
-  } else {
-    greeting = "Good evening";
-}
-
+    
+/* 
+To Do:
+Fix result
+Find out about ElseIf
+Put in info
+Comment code
+Slight CSS
+*/ 
