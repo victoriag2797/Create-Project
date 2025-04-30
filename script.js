356c4881736f984c745633447bebf2e71c271a5a
victoriag2797 <victoriag2797@tuckahoeschools.org>
2025-04-30 12:35:06 +0000

    last commit

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ let questionList = [
     "Pick one of my brother's favorite movie:", 
     "Pick a random word:"
 ];
+
 //List of answers for each question
 let optAnswers = [
     ["Seattle Kraken", "St. Louis Blues", "Washington Capitals", "Carolina Hurricanes"], 
@@ -28,7 +29,7 @@ let optAnswers = [
     ["Death", "Heights", "The Dark", "Spiders"],
     ["Winter", "Autumn", "Spring", "Summer"],
     ["Air", "Water", "Earth", "Fire"],
-    ["6+", "4-5", "1-3", "Only Child"],
+    ["5+", "3-4", "1-2", "Only Child"],
     ["Ambivert", "Extrovert", "Introvert", "No clue"], 
     ["Pasta", "Pizza", "Water", "Bagel"],
     ["Whiplash", "Inglorious Basterds", "Green Mile", "No Country for Old Men"],
@@ -52,7 +53,7 @@ let questionNumberContainer = document.getElementById('question-number');
 let score = 0;
 let questionIndex = 0;
 
-//
+//This function loops through four possible answers (0-3), finding the index of the selected answer to add that value to the score
 function gradeAnswer(answerIndex) {
     for (let i = 0; i < 4; i++){
         if (i === answerIndex) {
@@ -62,47 +63,38 @@ function gradeAnswer(answerIndex) {
     optFunct();
 }
 
+gradeAnswer(answerIndex);
 
+//
 function optFunct() {
-    if (questionIndex < questionList.length) {
-        questionNumberContainer.textContent = `Question Number: ${
-            questionIndex + 1
-        }`;
-        questionContainer.textContent = questionList[questionIndex];
+    if (questionIndex < questionList.length) { 
+        questionNumberContainer.textContent = `Question Number: ${questionIndex + 1}`;
+        questionContainer.textContent = questionList[questionIndex]; 
         answerContent[0].textContent = optAnswers[questionIndex][0];
         answerContent[1].textContent = optAnswers[questionIndex][1];
         answerContent[2].textContent = optAnswers[questionIndex][2];
         answerContent[3].textContent = optAnswers[questionIndex][3];
         questionIndex += 1;
-    } else {
-        questionSection.textContent = "";
-        scoreContainer.innerText = "Your score is ${score}/60";
-        if (score == 15) {
+    } else { //When the question index exceeds the amount of questions -->
+        questionSection.textContent = ""; //Set the content in questionSection to nothing
+        scoreContainer.innerText = `Your score is ${score}/60`; //Display the score
+        //For each range of the score a different result is shown
+        if (score === 16) {
             scoreContainer.innerText = "I fear you are literally me, exactly. Like we picked the exact same answers. You're perfect!";
-            } else if (score > 15 && score < 25) {
+            } else if (score > 16 && score < 25) {
                 scoreContainer.innerText = "We have really similar opinions and we would probably get along really well!";
-            } else if (score > 24 && score < 34) {
+            } else if (score >= 25 && score < 35) {
                 scoreContainer.innerText = "We are pretty similar. You have good taste (mostly).";
-            } else if (score > 34 && score < 44) {
+            } else if (score >= 35 && score < 45) {
                 scoreContainer.innerText = "Down the middle. Your opinions are half good, half bad.";
-            } else if (score > 43 && score < 53) {
+            } else if (score >= 45 && score < 55) {
                 scoreContainer.innerText = "We are pretty different. You have some good opinions though (Not a lot).";
-            } else if (score > 52 && score < 59) {
-                scoreContainer.innerText = "We're very different. You need to get some better opinions.";
-            } else (score == 60) {
-                scoreContainer.innerText = "We're exact opposites. We picked the opposite answer every time. You're heavily flawed (joking (mostly)).";
+            } else if (score >= 55 && score < 59) {
+                scoreContainer.innerText = "We're very different. You need to get some better opinions (joking).";
+            } else {
+                scoreContainer.innerText = "We're exact opposites. We picked the opposite answer every time. You're heavily flawed (joking) (mostly).";
             }
         }
 };
 
 optFunct();
-
-    
-/* 
-To Do:
-Fix result
-Find out about ElseIf
-Put in info
-Comment code
-Slight CSS
-*/ 
